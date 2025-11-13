afe161bc7f93944505f79d67dd1ff0420dc5502e
KhanAzlan <azlankhan7266@gmail.com>
2025-11-13 16:17:18 +0530

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0fa91bc90632c73c9"
   instance_type = "t3.micro"
 
   tags = {
